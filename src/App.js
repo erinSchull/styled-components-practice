5fac60ae1ae75a9ee033b7ab0ee34148a1a5234a
Erin Schull <erin.schull@gmail.com>
2018-03-28 19:31:06 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Wrapper from './components/Wrapper';
+
+const CustomTitle = styled.h1`
+    color: darkviolet;
+    font-size: 90px;
+    text-align: center;
+    font: "Calibri";
+`
+const Header = styled.section`
+  background-color: #f4f4f4;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 10px;
+`
+
+const Button = styled.button`
+  background-color: pink;
+  width: 250px;
+  height: 75px;
+  padding: 10px;
+  font-size: 30px;
+  border: solid 3px darkturquoise;
+  color: white;
+  position: relative;
+  left: 20%;
+  box-sizing: border-box;
+  text-align: center;
+`
+const LoremText = styled.p`
+  color: #000;
+  text-align: left;
+  padding-left: 10%;
+`
+
+class App extends Component {
+  render() {
+    console.log('this.props', this.props)
+    return (
+      <Wrapper>
+        <Header>
+          <CustomTitle>My custom title</CustomTitle>
+          <LoremText>Lorem ipsum dolor sit amet</LoremText>
+          <br />
+          <Button>Click Bait ;)</Button>
+        </Header>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 0px;
+    
+    position: absolute;
+    top: 0px;
+    width: 100%;
+    padding-right: 10px;
+    padding-left: 10px;
+    padding-top: 0px;
+    box-sizing: border-box;
+    height: 100vh;
+    background-color: #f4f4f4;
+`;
